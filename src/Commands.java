6fe9331b170de7897711f5462dfb6dd9657d02d3
Berg <vandenberg.jutai@gmail.com>
2017-02-12 02:48:53 -0400

    Adicao de comandos

--- src/Commands.java
+++ src/Commands.java
@@ -1,24 +1,25 @@
 import java.io.*;
 import java.sql.Connection;
+import java.util.Properties;
 
 /**
  * Created by berg on 06/02/17.
  */
 public class Commands {
-    public static String lvlShell = System.getProperty ("user.name");
+    public static String lvlShell = System.getProperty ("user.name") + "@:~";
     public static String currentDir = System.getProperty("user.home");
 
     public Commands() {
     }
 
     public String listCommands() {
-        return "-> pwd\n-> cat\n-> ls\n-> cd\n-> exit\n-> clear";
+        return "-> pwd\n-> cat\n-> ls\n-> cd\n-> cd ..\n-> exit\n-> clear\n-> rm -r\n-> mkdir\n-> mkdir -p\n-> mv";
     }
 
     public String cat(String file) {
         String r = "";
         try {
-            FileReader fileReader = new FileReader(file);
+            FileReader fileReader = new FileReader(currentDir + "/" + file);
             BufferedReader in = new BufferedReader(fileReader);
             String line;
             while((line = in.readLine())!= null){
@@ -36,7 +37,7 @@ public class Commands {
 
     public String ls() {
         String r = "";
-        File dir = new File(System.getProperty("user.dir"));
+        File dir = new File(currentDir);
         String childs[] = dir.list();
         for(String child: childs){
             if (r.isEmpty()) r += child;
@@ -46,15 +47,25 @@ public class Commands {
     }
 
     public String cd(String d) {
-        File dir = new File(d);
+        File dir = new File(currentDir + "/" + d);
         if(dir.isDirectory()) {
             System.setProperty("user.dir", dir.getAbsolutePath());
+            currentDir += "/" + d;
+            lvlShell += "/" + d;
             return d;
         } else {
             System.out.println(d + " is not a directory.");
         }
         return null;
     }
+
+    public void cdBack() {
+        int cut = lvlShell.lastIndexOf("/");
+        int cutDir = currentDir.lastIndexOf("/");
+        lvlShell = lvlShell.substring(0, cut);
+        currentDir = currentDir.substring(0,cutDir);
+    }
+
     public void clear() {
         for (int i = 0; i < 54; i++) {
             System.out.println("\n");
@@ -62,6 +73,39 @@ public class Commands {
     }
 
     public String pwd() {
-        return System.getProperty("user.dir");
+        return currentDir;
+    }
+
+    public void mkdir(String name, boolean subdir) {
+        try {
+            File dir = new File(currentDir + "/" + name);
+            if (subdir) dir.mkdirs();
+            else dir.mkdir();
+        } catch (Exception ex) {
+            System.out.println("Erro ao criar o diretorio");
+        }
+    }
+
+    public void rm(String path) {
+        File file = new File(path);
+        rm(file);
+    }
+
+    private void rm(File f) {
+        if (f.isDirectory()) {
+            File[] files = f.listFiles();
+            for (File file : files) {
+                rm(file);
+            }
+        }
+        f.delete();
+    }
+
+    public void mv(String from, String to) {
+        File arq = new File(currentDir + "/" + from);
+        // diretorio de destino
+        File dir = new File(to);
+        // move o arquivo para o novo diretorio
+        arq.renameTo(new File(dir, arq.getName()));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends JFrame {
+    private JTextArea jTextArea;
+
+    public Main() {
+        super("Terminal");
+        setSize(730, 410);
+//        this.jTextArea = new JTextArea();
+//        this.jTextArea.setLineWrap(true); //quebra de linha automática
+//        add(this.jTextArea);
+        Color c = Color.decode("#2C001E");
+        getContentPane().setBackground(c);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        System.out.println("Bem vindo ao prompt de comando!\nDigite 'info' para listar os comandos disponiveis" );
+
+//        final Main t = new Main();
+//        EventQueue.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                t.setVisible(true);
+//            }
+//        });
+
+        String welcome = "Bem vindo ao prompt de comando!\nDigite 'info' para listar os comandos disponiveis";
+        System.out.println(welcome);
+//        t.jTextArea.setText(welcome);
+//        t.add(t.jTextArea);
 
         Commands commands = new Commands();
-        System.out.print(Commands.lvlShell + "@:~$ ");
+        System.out.print(Commands.lvlShell + "$ ");
 
         String[] input = read.nextLine().split(" ");
         String p = "";
-        while (!input[0].toLowerCase().equals("exit")) {
+        while (true) {
             switch (input[0].toLowerCase()) {
                 case "info":
                     System.out.println(commands.listCommands());
@@ -27,18 +53,32 @@ public class Main {
                     System.out.println(commands.ls());
                     break;
                 case "cd":
-                    p = commands.cd(input[1]);
+                    if (input[1].equals(".."))
+                        commands.cdBack();
+                    else p = commands.cd(input[1]);
                     break;
                 case "clear":
                     commands.clear();
                     break;
+                case "rm":
+                    if (input[1].equals("-r")) commands.rm(input[2]);
+                    break;
+                case "mkdir":
+                    if (input[1].equals("-p")) commands.mkdir(input[2], true);
+                    else commands.mkdir(input[1], false);
+                    break;
+                case "mv":
+                    commands.mv(input[1], input[2]);
+                    break;
+                case "exit":
+                    System.exit(0);
                 default:
                     break;
             }
             if (p == null || p.isEmpty())
-                System.out.print(Commands.lvlShell + "@:~$ ");
+                System.out.print(Commands.lvlShell + "$ ");
             else
-                System.out.print(Commands.lvlShell + "@:~/" + p + "$ ");
+                System.out.print(Commands.lvlShell + "$ ");
 
             input = read.nextLine().split(" ");
         }
